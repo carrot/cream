8d9fb9e2838777d8458cbb3761546500334ac663
BrandonRomano <BrandonRRomano@gmail.com>
2014-05-30 15:06:26 -0400

    Added description.

--- src/main/java/com/carrotcreative/cream/cache/strategies/StandardCacheStrategy.java
+++ src/main/java/com/carrotcreative/cream/cache/strategies/StandardCacheStrategy.java
@@ -6,7 +6,25 @@ import com.carrotcreative.cream.cache.CacheStrategy;
 import com.carrotcreative.cream.cache.CacheStrategyCallback;
 import com.carrotcreative.cream.util.InternetStatus;
 
-/* TODO add description */
+/**
+ * ===== Standard Caching Strategy =====
+ *
+ *  -> Cache files when they are loaded from the API
+ *  -> Flag each type of file with a specific expiration, EXPIRATION_DAYS away
+ *  -> When the expiration date hits, we'll try to pull from the API again
+ *  -> if we have network availability
+ *          -> Hit the API
+ *          -> If that somehow ended up failing
+ *              -> Hit the cache with no regard to expiration
+ *     else
+ *          ->Hit the cache with no regard to expiration
+ *
+ *  -> Cache will be cleaned up after the file is TRASH_DAYS past expiration
+ *     to prevent the cache from filling up too much.
+ *
+ *  -> Expired files will also be replaced in the event there is a successful
+ *     API call to the same file.
+ */
 public class StandardCacheStrategy<T> implements CacheStrategy<T> {
 
     private Context mContext;
