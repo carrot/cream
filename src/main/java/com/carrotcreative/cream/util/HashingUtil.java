7118c26133ccc423d4edb86d26e4fdc3f4e32c5d
Kaushik Saurabh <kaushik@carrotcreative.com>
2015-05-18 11:01:13 -0400

    Add bytesToHex function
    put opening brace in its own line.

--- src/main/java/com/carrotcreative/cream/util/HashingUtil.java
+++ src/main/java/com/carrotcreative/cream/util/HashingUtil.java
@@ -9,6 +9,7 @@ import android.support.annotation.NonNull;
 public class HashingUtil
 {
     private static final int CACHE_SIZE = 50;
+    private static final char[] hexArray = "0123456789ABCDEF".toCharArray();
     private static LruCache<String, String> mCache = new LruCache<>(CACHE_SIZE);
 
     private HashingUtil() {}
@@ -37,7 +38,7 @@ public class HashingUtil
         {
             MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
             messageDigest.update(secret.getBytes("UTF-8"));
-            hashedString = new String(messageDigest.digest());
+            hashedString = bytesToHex(messageDigest.digest());
         }
         catch (NoSuchAlgorithmException e) { /* Do nothing */ }
         catch (UnsupportedEncodingException e) { /* Do nothing */ }
@@ -47,6 +48,17 @@ public class HashingUtil
             hashedString = secret.replaceAll("\\s+", "_");
         }
         return hashedString;
+    }
 
+    public static String bytesToHex(byte[] bytes)
+    {
+        char[] hexChars = new char[bytes.length * 2];
+        for ( int j = 0; j < bytes.length; j++ )
+        {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
     }
 }
