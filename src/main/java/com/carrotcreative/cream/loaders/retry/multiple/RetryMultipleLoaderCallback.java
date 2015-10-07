2fb6c0d9e1d27cd40b331bd84ef9607aac7617f4
Kaushik Saurabh <kaushik@carrotcreative.com>
2015-10-07 16:14:41 -0400

    add MultipleLoaderTuple for RetryMultipleLoaderCallback

--- src/main/java/com/carrotcreative/cream/loaders/retry/multiple/RetryMultipleLoaderCallback.java
+++ src/main/java/com/carrotcreative/cream/loaders/retry/multiple/RetryMultipleLoaderCallback.java
@@ -1,10 +1,12 @@
 package com.carrotcreative.cream.loaders.retry.multiple;
 
+import com.carrotcreative.cream.loaders.multiple.MultipleLoaderTuple;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 
 public interface RetryMultipleLoaderCallback<Content extends Serializable> {
-    void success(ArrayList<Content> content);
+    void success(ArrayList<MultipleLoaderTuple<Content>> content);
     void failedAttempt(int attemptNumber);
     void always();
 }
