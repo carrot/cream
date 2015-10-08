c60d161028d62dfe2af8b417536aa932698b75e0
Kaushik Saurabh <kaushik@carrotcreative.com>
2015-10-08 11:30:32 -0400

    make MutlipleLoaderTuple variables private

--- src/main/java/com/carrotcreative/cream/loaders/multiple/MultipleLoaderTuple.java
+++ src/main/java/com/carrotcreative/cream/loaders/multiple/MultipleLoaderTuple.java
@@ -4,13 +4,23 @@ import java.io.Serializable;
 
 public class MultipleLoaderTuple<Content extends Serializable> {
 
-    public Content mContent;
-    public boolean mFromCache;
+    private Content mContent;
+    private boolean mFromCache;
 
     public MultipleLoaderTuple(Content content, boolean fromCache)
     {
         mContent = content;
         mFromCache = fromCache;
+    }
+
+    public Content getContent()
+    {
+        return mContent;
+    }
+
+    public boolean isFromCache()
+    {
+        return mFromCache;
     }
 
 }
